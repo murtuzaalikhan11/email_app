e3a1bbb2ebac59b29e247273cbf72d0c7d32d092
murtuza ali khan <murtuzaalikhan737@gmail.com>
2025-06-06 02:59:28 +0530

    Remove client and server folders from GitHub only

--- server/routes/emailrouter.js
+++
@@ -1,9 +0,0 @@
-const emailctrl=require("../controllers/emailctrl") 
-const isauth=require("../middleware/isauth")
-const router=require("express").Router() 
-
-router.post("/createemail", isauth, emailctrl.createemail)
-router.post("/getemails", isauth, emailctrl.getemails)
-router.delete("/:id", isauth, emailctrl.deleteemail)
-
-module.exports=router
--- server/routes/userrouter.js
+++
@@ -1,9 +0,0 @@
-
-const usercontroller=require("../controllers/usercontroller") 
-const router=require("express").Router() 
-
-router.post("/register",usercontroller.register)
-router.post("/login",usercontroller.login) 
-router.post("/logout",usercontroller.logout)
-
-module.exports=router
